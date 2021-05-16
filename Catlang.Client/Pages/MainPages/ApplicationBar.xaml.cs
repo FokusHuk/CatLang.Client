94e59b45a12cbbd60165313f7f8f77a612e30ff4
Ilya Axenov <31668898+FokusHuk@users.noreply.github.com>
2021-05-17 00:03:01 +0300

    main pages configured

--- Catlang.Client/Pages/MainPages/ApplicationBar.xaml.cs
+++ Catlang.Client/Pages/MainPages/ApplicationBar.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows.Controls;
+using System;
 
 namespace Catlang.Client.Pages.MainPages
 {
@@ -7,11 +8,34 @@ namespace Catlang.Client.Pages.MainPages
     /// </summary>
     public partial class ApplicationBar : Page
     {
-        public ApplicationBar()
+        private Action OpenSetsPage;
+        private Action OpenRecommendationPage;
+        private Action OpenProfilePage;
+
+        public ApplicationBar(Action openSetsPage, Action openRecommendationPage, Action openProfilePage)
         {
             InitializeComponent();
 
+            OpenSetsPage = openSetsPage;
+            OpenRecommendationPage = openRecommendationPage;
+            OpenProfilePage = openProfilePage;
+
             Username.Text = CatLangRestClient.Username;
+        }
+
+        private void Sets_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            OpenSetsPage();
+        }
+
+        private void Recommendation_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            OpenRecommendationPage();
+        }
+
+        private void Profile_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            OpenProfilePage();
         }
     }
 }
--- Catlang.Client/Pages/MainPages/Main.xaml.cs
+++ Catlang.Client/Pages/MainPages/Main.xaml.cs
@@ -9,13 +9,36 @@ namespace Catlang.Client.Pages
     public partial class Main : Page
     {
         ApplicationBar applicationBar;
+        SetsPage setsPage;
+        RecommendationPage recommendationPage;
+        ProfilePage profilePage;
 
         public Main()
         {
             InitializeComponent();
 
-            applicationBar = new ApplicationBar();
+            applicationBar = new ApplicationBar(() => OpenSetsPage(), () => OpenRecommendationPage(), () => OpenProfilePage());
+            setsPage = new SetsPage();
+            recommendationPage = new RecommendationPage();
+            profilePage = new ProfilePage();
+
             appBar.Content = applicationBar;
+            body.Content = setsPage;
+        }
+
+        private void OpenSetsPage()
+        {
+            body.Content = setsPage;
+        }
+
+        private void OpenRecommendationPage()
+        {
+            body.Content = recommendationPage;
+        }
+
+        private void OpenProfilePage()
+        {
+            body.Content = profilePage;
         }
     }
 }
