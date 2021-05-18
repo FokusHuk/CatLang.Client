1867f69308efdec144b3ba97e49083d3653b0f85
Ilya Axenov <31668898+FokusHuk@users.noreply.github.com>
2021-05-18 23:17:17 +0300

    sets page added without exercises

--- Catlang.Client/Pages/MainPages/SetsPage.xaml.cs
+++ Catlang.Client/Pages/MainPages/SetsPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,66 @@ namespace Catlang.Client.Pages.MainPages
     /// </summary>
     public partial class SetsPage : Page
     {
+
         public SetsPage()
         {
             InitializeComponent();
+
+            DataContext = new View();
         }
+    }
+
+    public class View
+    {
+        public ObservableCollection<Set> Sets { get; set; }
+
+        public View()
+        {
+            Sets = new ObservableCollection<Set>()
+            {
+                new Set(Guid.NewGuid(), "Ghoul", "Memories", "love, dead, robots, inside...", 12, 5, 1, 993),
+                new Set(Guid.NewGuid(), "Admin", "Simple", "true, false, mthfckr...", 1, 1, 1, 1)
+            };
+        }
+    }
+
+    public class Set
+    {
+        public Set(Guid id, string authorName, string studyTopic, string words, int popularity, double efficiency, double averageStudyTime, double complexity)
+        {
+            Id = id;
+            AuthorName = authorName;
+            StudyTopic = studyTopic;
+            Words = words;
+            Popularity = popularity;
+            Efficiency = efficiency;
+            AverageStudyTime = averageStudyTime;
+            Complexity = complexity;
+        }
+
+        public Guid Id { get; set; }
+        public string AuthorName { get; set; }
+        public string StudyTopic { get; set; }
+        public string Words { get; set; }
+        public int Popularity { get; set; }
+        public double Efficiency { get; set; }
+        public double AverageStudyTime { get; set; }
+        public double Complexity { get; set; }
+    }
+
+    public class Word
+    {
+        public Word(int id, string original, string translation)
+        {
+            Id = id;
+            Original = original;
+            Translation = translation;
+        }
+
+        public int Id { get; }
+
+        public string Original { get; set; }
+
+        public string Translation { get; set; }
     }
 }
