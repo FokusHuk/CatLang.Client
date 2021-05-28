a5071f593f5c7414c4ce065c670aa25a9ade77fb
Ilya Axenov <31668898+FokusHuk@users.noreply.github.com>
2021-05-28 20:07:22 +0300

    profile page studied sets layout

---
+++ Catlang.Client/Models/StudiedSet.cs
@@ -0,0 +1,20 @@
+﻿namespace Catlang.Client.Models
+{
+    public class StudiedSet
+    {
+        public StudiedSet(string studyTopic, string authorName, string status, string attemptsCount, string correctAnswers)
+        {
+            StudyTopic = studyTopic;
+            AuthorName = authorName;
+            Status = status;
+            AttemptsCount = attemptsCount;
+            CorrectAnswers = correctAnswers;
+        }
+
+        public string StudyTopic { get; set; }
+        public string AuthorName { get; set; }
+        public string Status { get; set; }
+        public string AttemptsCount { get; set; }
+        public string CorrectAnswers { get; set; }
+    }
+}
--- Catlang.Client/Pages/MainPages/ProfilePage.xaml.cs
+++ Catlang.Client/Pages/MainPages/ProfilePage.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using Catlang.Client.Models;
+using System.Collections.ObjectModel;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Catlang.Client.Pages.MainPages
 {
@@ -20,9 +9,36 @@ namespace Catlang.Client.Pages.MainPages
     /// </summary>
     public partial class ProfilePage : Page
     {
+        ProfilePageView view;
+
         public ProfilePage()
         {
             InitializeComponent();
+
+            view = new ProfilePageView(new ObservableCollection<StudiedSet>()
+            {
+                new StudiedSet("Topic", "Name", "Изучено", "10", "10 из 12"),
+                new StudiedSet("Topic", "Name", "Изучено", "10", "10 из 12"),
+                new StudiedSet("Topic", "Name", "Изучено", "10", "10 из 12"),
+                new StudiedSet("Topic", "Name", "Изучено", "10", "10 из 12"),
+                new StudiedSet("Topic", "Name", "Изучено", "10", "10 из 12"),
+                new StudiedSet("Topic", "Name", "Изучено", "10", "10 из 12"),
+                new StudiedSet("Topic", "Name", "Изучено", "10", "10 из 12")
+            });
+
+            DataContext = view;
+        }
+
+
+    }
+
+    public class ProfilePageView
+    {
+        public ObservableCollection<StudiedSet> UsedSets { get; set; }
+
+        public ProfilePageView(ObservableCollection<StudiedSet> usedSets)
+        {
+            UsedSets = usedSets;
         }
     }
 }
