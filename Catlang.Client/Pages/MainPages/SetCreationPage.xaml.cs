199642ab5cfb5dfec20551f31139d4bea400a6b1
Ilya Axenov <31668898+FokusHuk@users.noreply.github.com>
2021-05-29 18:43:50 +0300

    set creation page left panel layout

--- Catlang.Client/Pages/MainPages/SetCreationPage.xaml.cs
+++ Catlang.Client/Pages/MainPages/SetCreationPage.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Catlang.Client.Models;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +22,48 @@ namespace Catlang.Client.Pages.MainPages
     /// </summary>
     public partial class SetCreationPage : Page
     {
+        SetCreationPageView view;
+
         public SetCreationPage()
         {
             InitializeComponent();
+
+            view = new SetCreationPageView(
+                new ObservableCollection<Word>()
+                {
+                    new Word(1, "Word1", "Translation"),
+                    new Word(2, "Word2", "Translation"),
+                    new Word(3, "Word3", "Translation"),
+                    new Word(4, "Word4", "Translation"),
+                    new Word(5, "Word5", "Translation"),
+                    new Word(6, "Word6", "Translation"),
+                    new Word(7, "Word7", "Translation")
+                });
+            DataContext = view;
+        }
+
+        private void RemoveWord_Click(object sender, RoutedEventArgs e)
+        {
+            var button = sender as Button;
+            var word = button.DataContext as Word;
+            view.SetWords.Remove(word);
+        }
+    }
+
+    public class SetCreationPageView
+    {
+        public Word SelectedItem { get; set; }
+        public ObservableCollection<Word> SetWords { get; set; }
+
+        public SetCreationPageView(ObservableCollection<Word> setWords)
+        {
+            SetWords = setWords;
+        }
+
+        public SetCreationPageView(SetCreationPageView view)
+        {
+            SelectedItem = null;
+            SetWords = view.SetWords;
         }
     }
 }
